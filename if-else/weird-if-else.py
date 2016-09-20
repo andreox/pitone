3fbd7812437526f0e41f4db9381d7ad0f2357de4
andreox <andreozzi.alessio@gmail.com>
2016-09-20 22:33:35 +0200

    Create weird-if-else.py

---
+++ if-else/weird-if-else.py
@@ -0,0 +1,15 @@
+#!/bin/python
+
+N = int(raw_input())
+
+if N % 2 != 0 :
+	print 'Weird'
+
+elif N >= 2 and N <= 5 :
+	print 'Not Weird'
+
+elif N >= 6 and N <= 20 :
+	print 'Weird'
+
+else :
+	print 'Not Weird'
